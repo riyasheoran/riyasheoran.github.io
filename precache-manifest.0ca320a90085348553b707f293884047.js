9e42f97966933372bc42833238164ca0a9e31253
riyasheoran <riyasheoran001@gmail.com>
2020-11-07 16:12:29 +0530

    Updates

---
+++ precache-manifest.0ca320a90085348553b707f293884047.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcab3b4d49c1d1d4b6f2",
+    "url": "/wallstreet/static/css/main.044e3f56.chunk.css"
+  },
+  {
+    "revision": "fcab3b4d49c1d1d4b6f2",
+    "url": "/wallstreet/static/js/main.fcab3b4d.chunk.js"
+  },
+  {
+    "revision": "bb52a641ea255d5ebb1b",
+    "url": "/wallstreet/static/js/1.bb52a641.chunk.js"
+  },
+  {
+    "revision": "2d5f33fd11b61054a8a9",
+    "url": "/wallstreet/static/js/runtime~main.2d5f33fd.js"
+  },
+  {
+    "revision": "ac3406b170921e91a5949023073f0c1e",
+    "url": "/wallstreet/index.html"
+  }
+];
